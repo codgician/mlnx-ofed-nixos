79fad3d0c2dd049851d10a59cc14b83183b86cc2
codgician <15964984+codgician@users.noreply.github.com>
2025-03-16 15:42:39 +0800

    kernelPackages.fwctl: init

---
+++ pkgs/kernel-packages/fwctl/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  pkgs,
+  stdenv,
+  kernel,
+  kernelModuleMakeFlags,
+  mkUnpackScript,
+  bash,
+  mlnx-ofed-kernel,
+  mlnx-ofed-src,
+  ...
+}:
+let
+  kernelModuleInstallFlags = [ "INSTALL_MOD_PATH=${placeholder "out"}" ];
+  kernelDir = "${kernel.dev}/lib/modules/${kernelVersion}";
+  kernelVersion = kernel.modDirVersion;
+  mlnxOfedKernel = mlnx-ofed-kernel.override {
+    inherit kernel kernelModuleMakeFlags;
+    copySource = true;
+  };
+in
+stdenv.mkDerivation rec {
+  pname = "fwctl";
+  inherit (mlnx-ofed-src) src version;
+
+  unpackPhase = mkUnpackScript pname;
+
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  patchPhase = ''
+    patchShebangs .
+
+    substituteInPlace ./makefile \
+      --replace-warn '/bin/ls' 'ls' \
+      --replace-warn '/bin/bash' '${lib.getExe bash}'
+  '';
+
+  enableParallelBuilding = true;
+
+  makeFlags =
+    kernelModuleMakeFlags
+    ++ kernelModuleInstallFlags
+    ++ [
+      "OFA_DIR=${mlnxOfedKernel}/usr/src/ofa_kernel"
+      "K_BUILD=${kernelDir}/build"
+    ];
+
+  installFlags = kernelModuleInstallFlags;
+
+  meta = with pkgs.lib; {
+    description = "Mellanox fwctl module for managing adapter firmware";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ codgician ];
+  };
+}
