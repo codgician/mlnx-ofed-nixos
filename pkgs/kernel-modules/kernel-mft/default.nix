6f581cf7ada3cfb51d46c4dc946890ccd06d6cb4
codgician <15964984+codgician@users.noreply.github.com>
2025-03-17 12:01:07 +0800

    kernelModules.kernel-mft: init

---
+++ pkgs/kernel-modules/kernel-mft/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  pkgs,
+  stdenv,
+  kernel,
+  mkUnpackScript,
+  mlnx-ofed-src,
+  ...
+}:
+let
+  kernelDir = "${kernel.dev}/lib/modules/${kernelVersion}";
+  kernelVersion = kernel.modDirVersion;
+in
+stdenv.mkDerivation rec {
+  pname = "kernel-mft";
+  inherit (mlnx-ofed-src) src version;
+
+  unpackPhase = mkUnpackScript pname;
+
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  patchPhase = ''
+    patchShebangs .
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    make -j$NIX_BUILD_CORES \
+      CPUARCH=${pkgs.system} \
+      KSRC=${kernelDir}/build \
+      KPVER=${kernelVersion}
+
+    runHook postBuild
+  '';
+
+  installPhase =
+    ''
+      install_dir=$out/lib/modules/${kernelVersion}/updates
+      mkdir -p $install_dir
+      cp ./mst_backward_compatibility/mst_pci/mst_pci.ko $install_dir
+      cp ./mst_backward_compatibility/mst_pciconf/mst_pciconf.ko $install_dir
+    ''
+    + lib.optionalString (stdenv.hostPlatform.isPower) ''
+      cp ./mst_backward_compatibility/mst_ppc/mst_ppc_pci_reset.ko $install_dir
+    ''
+    + lib.optionalString (stdenv.hostPlatform.isAarch64) ''
+      cp ./misc_drivers/bf3_livefish/bf3_livefish.ko $install_dir
+    '';
+
+  meta = with pkgs.lib; {
+    description = "Mellanox kernel-mft module";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ codgician ];
+  };
+}
