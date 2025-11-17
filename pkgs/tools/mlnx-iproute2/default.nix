d1806181b54375c2de3bf1cd9c7fa962c1a75647
codgician <15964984+codgician@users.noreply.github.com>
2025-11-17 14:51:15 +0800

    mlnx-iproute2: explictly define patches from nixpkgs

--- pkgs/tools/mlnx-iproute2/default.nix
+++ pkgs/tools/mlnx-iproute2/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  fetchurl,
   iproute2,
   mkUnpackScript,
   mlnx-ofed-src,
@@ -11,6 +12,19 @@ iproute2.overrideAttrs (oldAttrs: rec {
   inherit (mlnx-ofed-src) src version;
 
   unpackPhase = mkUnpackScript pname;
+
+  patches = [
+    (fetchurl {
+      name = "musl-endian.patch";
+      url = "https://lore.kernel.org/netdev/20240712191209.31324-1-contact@hacktivis.me/raw";
+      hash = "sha256-MX+P+PSEh6XlhoWgzZEBlOV9aXhJNd20Gi0fJCcSZ5E=";
+    })
+    (fetchurl {
+      name = "musl-basename.patch";
+      url = "https://lore.kernel.org/netdev/20240804161054.942439-1-dilfridge@gentoo.org/raw";
+      hash = "sha256-47obv6mIn/HO47lt47slpTAFDxiQ3U/voHKzIiIGCTM=";
+    })
+  ];
 
   meta =
     with lib;
