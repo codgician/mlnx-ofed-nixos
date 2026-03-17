cd91f3d7bf7a52af5900d4026fdf6a5ac42d98b2
codgician <15964984+codgician@users.noreply.github.com>
2026-03-17 18:22:53 +0800

    iser/srp: init

---
+++ pkgs/kernel-modules/iser/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  stdenv,
+  kernel,
+  kernelModuleMakeFlags,
+  kernelModuleInstallFlags,
+  mkUnpackScript,
+  mlnx-ofed-kernel,
+  mlnx-ofed-src,
+  ...
+}:
+let
+  kernelDir = "${kernel.dev}/lib/modules/${kernelVersion}";
+  kernelVersion = kernel.modDirVersion;
+  mlnxOfedKernel = mlnx-ofed-kernel.override {
+    inherit kernel kernelModuleMakeFlags;
+    copySource = true;
+  };
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "iser";
+  inherit (mlnx-ofed-src) src version;
+
+  unpackPhase = mkUnpackScript finalAttrs.pname;
+
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  postPatch = ''
+    substituteInPlace ./makefile \
+      --replace-fail '/bin/ls' 'ls' \
+      --replace-fail '/bin/bash' 'bash'
+    patchShebangs .
+  '';
+
+  enableParallelBuilding = true;
+
+  makeFlags = kernelModuleMakeFlags ++ [
+    "OFA_DIR=${mlnxOfedKernel}/src/ofa_kernel/${kernelVersion}"
+    "K_BUILD=${kernelDir}/build"
+  ];
+
+  installFlags = kernelModuleInstallFlags;
+
+  meta = with lib; {
+    description = "iSCSI Extensions for RDMA (iSER) initiator kernel module";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ codgician ];
+  };
+})
